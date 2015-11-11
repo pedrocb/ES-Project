598ba7581de9e086f7b6a9cf41ae1d61ef498f6d
pedrocb <pedrocb_c-s@hotmail.com>
2015-11-11 18:24:47 +0000

    fixed merge

--- dashboard/ditic_kanban/config.py
+++ dashboard/ditic_kanban/config.py
@@ -101,11 +101,7 @@ class DITICConfig:
             'deleted',
         ]
         self.system = {
-<<<<<<< HEAD
             'working_dir': '/home/pedro/kLeI/ES/Project/ES-Project/dashboard/ditic_kanban/',
-=======
-            'working_dir': '/home/godinho/Documents/ES-Git/ES-Project/dashboard/ditic_kanban/',
->>>>>>> 5565f1f201c37ccb5e3d16747e37e5915b750cdd
             'summary_file': 'summary',
             'server': 'localhost:4200',
             'username': 'root',
